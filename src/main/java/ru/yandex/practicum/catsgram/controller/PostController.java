9b93bb95087e98386bb23f54549b928317f9771a
egopv <egop.valuev@yandex.ru>
2025-06-09 23:41:12 +0300

    Update controller

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
 
 import java.time.Instant;
@@ -42,5 +43,23 @@ public class PostController {
                 .max()
                 .orElse(0);
         return ++currentMaxId;
+    }
+
+    @PutMapping
+    public Post update(@RequestBody Post newPost) {
+        // проверяем необходимые условия
+        if (newPost.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (posts.containsKey(newPost.getId())) {
+            Post oldPost = posts.get(newPost.getId());
+            if (newPost.getDescription() == null || newPost.getDescription().isBlank()) {
+                throw new ConditionsNotMetException("Описание не может быть пустым");
+            }
+            // если публикация найдена и все условия соблюдены, обновляем её содержимое
+            oldPost.setDescription(newPost.getDescription());
+            return oldPost;
+        }
+        throw new NotFoundException("Пост с id = " + newPost.getId() + " не найден");
     }
 }
---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,95 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    // Вспомогательный метод для генерации ID нового пользователя
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        // Проверка обязательных условий
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Email должен быть указан");
+        }
+
+        // Проверка уникальности email
+        boolean emailExists = users.values()
+                .stream()
+                .anyMatch(u -> u.getEmail().equals(user.getEmail()));
+
+        if (emailExists) {
+            throw new DuplicatedDataException("Этот email уже используется");
+        }
+
+        // Установка дополнительных полей
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+
+        // Сохранение пользователя
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        // Проверка обязательных полей
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("ID должен быть указан");
+        }
+
+        // Поиск существующего пользователя
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+            // Проверка и обновление email
+            if (newUser.getEmail() != null && !newUser.getEmail().equals(oldUser.getEmail())) {
+                boolean emailExists = users.values()
+                        .stream()
+                        .anyMatch(u -> u.getEmail().equals(newUser.getEmail()));
+                if (emailExists) {
+                    throw new DuplicatedDataException("Этот email уже используется");
+                }
+                oldUser.setEmail(newUser.getEmail());
+            }
+
+            // Обновление остальных полей
+            if (newUser.getUsername() != null) {
+                oldUser.setUsername(newUser.getUsername());
+            }
+
+            if (newUser.getPassword() != null) {
+                oldUser.setPassword(newUser.getPassword());
+            }
+
+            return oldUser;
+        }
+
+        throw new NotFoundException("Пользователь с ID = " + newUser.getId() + " не найден");
+    }
+}
