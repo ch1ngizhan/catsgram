d53c4fcd9b4ccc186245344b9b89175b76a63924
egopv <egop.valuev@yandex.ru>
2025-08-04 23:43:35 +0300

    pq1

---
+++ src/main/java/ru/yandex/practicum/catsgram/dal/UserRepository.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.catsgram.dal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.catsgram.dal.mappers.UserRowMapper;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+    private final JdbcTemplate jdbc;
+    private final UserRowMapper mapper;
+
+    public List<User> findAll() {
+        String query = "SELECT * FROM users";
+        return jdbc.query(query, mapper);
+    }
+}
--- src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
@@ -1,24 +1,33 @@
 package ru.yandex.practicum.catsgram.service;
 
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.dal.UserRepository;
+import ru.yandex.practicum.catsgram.dto.UserDto;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
 import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.mapper.UserMapper;
 import ru.yandex.practicum.catsgram.model.User;
 
 import java.time.Instant;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
     private final Map<Long, User> users = new HashMap<>();
+    private final UserRepository userRepository;
 
-    public Collection<User> findAll() {
-        return users.values();
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserDto> getUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserMapper::mapToUserDto)
+                .collect(Collectors.toList());
     }
 
     public Optional<User> findById(long userId) {
